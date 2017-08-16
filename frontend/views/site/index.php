b6032696c648a4e2994067d99930e4cda8a6e618
rurarera1 <s16146@st.kic.ac.jp>
2017-08-17 00:07:47 +0900

    I added search box at the home page

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -11,19 +11,20 @@ $this->title = ' HOME-Welcome';
 
         <p class="lead">We care about you!</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="cart.php">Start shopping</a></p>
+      <div>  <form method="post">
+        		    <input type="text" name="t1" value="search" onfocus="if(this.value=='search'){this.value='';}"onBlur="if(this.value==''){this.value='search';}"/>
+                    <input name="sear" type="submit" class="button" id="sub" value="Go" />
+        </form></div>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Who we are...</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>The Rurangwa Rexvirt Shop (RR shop), it is an ELECTRONICS and SOFTWARE company bases at Kigali,RWAJA. It was launched in 2017 from paternship of Japanese company and Rwandese one.</p>
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>
