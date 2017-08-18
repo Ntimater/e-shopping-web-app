83d0a17eae0d96a2ca95923d1be6bc4b28deee61
rurarera1 <s16146@st.kic.ac.jp>
2017-08-19 02:00:54 +0900

    changed header

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -11,7 +11,7 @@ $this->title = 'DASHBOARD';
 
         <p class="lead">You have successfully created your DASHBOARD</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="View SHOPS">Get started with Yii</a></p>
     </div>
 
     <div class="body-content">
@@ -29,11 +29,6 @@ $this->title = 'DASHBOARD';
             </div>
             <div class="col-lg-4">
                 <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
             </div>
