2fc9266a7c7a65dce391942f3a61fc3ead9a944b
rurarera1 <s16146@st.kic.ac.jp>
2017-08-19 02:27:12 +0900

    Body ph changed

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -18,7 +18,7 @@ $this->title = 'DASHBOARD';
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Our goods:</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
